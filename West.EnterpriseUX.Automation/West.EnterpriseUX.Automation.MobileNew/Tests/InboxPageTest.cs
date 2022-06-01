e194558404a42d51db43aebc1b497b4551679701
Girishwar Patil <Girishwar.PatilEXTERNAL@westpharma.com>
2022-06-01 13:30:20 +0000

    Merged PR 53208: Added TC [252668]
    Added TC [252668]
    
    Related work items: #252668, #534745, #534801, #534806

--- West.EnterpriseUX.Automation/West.EnterpriseUX.Automation.MobileNew/Tests/InboxPageTest.cs
+++ West.EnterpriseUX.Automation/West.EnterpriseUX.Automation.MobileNew/Tests/InboxPageTest.cs
@@ -125,48 +125,5 @@ namespace West.EnterpriseUX.Automation.MobileNew
                 Console.WriteLine(ex.Message);
             }
         }
-
-        [TestMethod]
-        [TestCategory("InboxPageTest")]
-        [Description("Verifying navigation to Child Inbox Page;")]
-        [Owner("Girishwar.PatilEXTERNAL@westpharma.com")]
-        [DynamicData(nameof(DataTransfer.InboxDataObject), typeof(DataTransfer), DynamicDataSourceType.Method)]
-        public void TC_252666_NavigationToChildInboxPageTest(string persona, string inbox, string searchRecord)
-        {
-            try
-            {
-                InboxPage inboxPage = _basePageInstance.NavigateToInboxPage();
-                DetailsPage detailsPage = inboxPage.NavigateToInboxByGlobalSearch(persona, inbox);
-
-                WaitForLoaderToDisappear(_basePageInstance.LoaderImage);
-
-                string firstWidgetTextValues = detailsPage.GetFirstWidgetTextValues().Trim().ToLower();
-                SemanticPage semanticPage = detailsPage.ClickOnViewDetails();
-
-                WaitForMoment(5);
-
-                _basePageInstance.VerifyCommonElementsDisplayedOrNot();
-                semanticPage.VerifyInboxMenuTitle(firstWidgetTextValues);
-                semanticPage.VerifyAllTheTabsAreDisplayedOrNot();
-                semanticPage.SelectChildInbox("Sales Order Items");
-
-                WaitForMoment(5);
-
-                Assert.AreEqual("Sales Order Items", inboxPage.InboxNameText.Text.Trim());
-                Assert.IsTrue(detailsPage.DetailsAbstractionTabTitle.Displayed);
-                Assert.IsTrue(inboxPage.OpenKpisAbstraction().KpisAbstractionTabTitle.Selected);
-                Assert.IsTrue(inboxPage.OpenChartsAbstraction().ChartsAbstractionTabTitle.Selected);
-                Assert.IsTrue(inboxPage.OpenStoryboardsAbstraction().StoryboardsAbstractionTabTitle.Selected);
-
-                detailsPage.DetailsAbstractionTabTitle.Click();
-                WaitForMoment(10);
-                Assert.IsNotNull(detailsPage.GetFirstWidgetTextValues());
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-                Console.WriteLine(ex.Message);
-            }
-        }
     }
 }
